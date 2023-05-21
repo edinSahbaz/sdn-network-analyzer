a98229493bd0f465577845db6705447e223f75c6
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-05-21 19:41:10 +0200

    Added controller

---
+++ controller.py
@@ -0,0 +1,59 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+
+
+class MyRyuApp(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(MyRyuApp, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Install a flow entry to send all packets to the controller
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Construct a flow mod message to add the flow entry
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        # Parse the incoming packet
+        pkt = packet.Packet(msg.data)
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)
+
+        # Process the packet as desired
+        # For example, you can print some information about the packet
+        print(f"Received packet: {eth_pkt}")
+
+        # Send the packet back out through the same switch
+        actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions)
+        datapath.send_msg(out)
--- main.py
+++ main.py
@@ -1,27 +1,57 @@
-import socket
-import struct
-import textwrap
+from mininet.net import Mininet
+from mininet.topo import Topo
+import networkx as nx
+import matplotlib.pyplot as plt
 
+def create_topology(mininet_net):
+    # Create an empty directed graph using NetworkX
+    topology = nx.DiGraph()
+
+    # Iterate over the hosts in the Mininet network and add them to the topology graph
+    for host in mininet_net.hosts:
+        topology.add_node(host.name, type='host', ip=host.IP())
+
+    # Iterate over the switches in the Mininet network and add them to the topology graph
+    for switch in mininet_net.switches:
+        topology.add_node(switch.name, type='switch')
+
+    # Iterate over the links in the Mininet network and add them to the topology graph
+    for link in mininet_net.links:
+        src_node = link.intf1.node
+        dst_node = link.intf2.node
+        src_port = link.intf1.name.split('-')[-1]
+        dst_port = link.intf2.name.split('-')[-1]
+        topology.add_edge(src_node, dst_node, src_port=src_port, dst_port=dst_port)
+
+    return topology
+
+def display_topology(topology):
+    # Draw the topology graph using NetworkX and Matplotlib
+    pos = nx.spring_layout(topology)
+    host_nodes = [node for node, attr in topology.nodes(data=True) if attr['type'] == 'host']
+    switch_nodes = [node for node, attr in topology.nodes(data=True) if attr['type'] == 'switch']
+    nx.draw_networkx_nodes(topology, pos, nodelist=host_nodes, node_color='r', node_size=500, alpha=0.8)
+    nx.draw_networkx_nodes(topology, pos, nodelist=switch_nodes, node_color='b', node_size=500, alpha=0.8)
+    nx.draw_networkx_edges(topology, pos, width=1.0, alpha=0.5)
+    nx.draw_networkx_labels(topology, pos, font_size=10, font_color='w')
+    plt.axis('off')
+    plt.show()
 
 def main():
-    con = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    # Create a Mininet network
+    net = Mininet(topo=Topo())
 
-    while True:
-        raw_data, addr = con.recvfrom(65536)
-        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
-        print('\nEthernet Frame:')
-        print('\nDestination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+    # Start the network
+    net.start()
 
+    # Create the network topology
+    topology = create_topology(net)
 
-def ethernet_frame(data):
-    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
-    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+    # Display the network topology
+    display_topology(topology)
 
+    # Stop the network
+    net.stop()
 
-def get_mac_addr(bytes_addr):
-    bytes_str = map('{02x}'.format, bytes_addr)
-    return ':'.join(bytes_str).upper()
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
